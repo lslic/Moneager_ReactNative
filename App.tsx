dbbb2650b7b6e0a09af2c301b1a779a8a5ed3fcd
liviuxyz-ctrl <livi2002liviu@gmail.com>
2023-01-31 10:56:38 +0200

    Adding necesary confing in babel file for react-native-reanimated

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+
+  const [count, setCounter] = useState(0)
+
+  const incrementCounter = () => {
+    setCounter(count + 1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to {count}</Text>
       <StatusBar style="auto" />
     </View>
   );

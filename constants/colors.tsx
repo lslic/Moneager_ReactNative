a4db29127d7c93cf4105291a490b2c5d3b710609
liviuxyz-ctrl <liviu2002liviu@gmail.com>
2023-03-20 18:51:48 +0200

    Adding files

--- constants/colors.tsx
+++ constants/colors.tsx
@@ -1,6 +1,7 @@
 // colors.ts
 
 export type colors = string;
+export type PrimaryColors = string;
 
 // colors.js
 
@@ -9,3 +10,18 @@ export const colors = {
 	SECONDARY_COLOR: '#FF3B30',
 	BACKGROUND_COLOR: '#F5F5F5',
 };
+
+
+export const PrimaryColors = {
+	PC_800: '#40356F',
+	PC_600: '#6E62A1',
+	PC_400: '#A9A1CC',
+}
+
+export const NeutralColors = {
+	NC_1200: '#18171F',
+	NC_900: '#49465F',
+	NC_700: '#A4A0C0',
+	NC_300: '#D2D3D3',
+	NC_WHITE: '#F6F6F6',
+}
